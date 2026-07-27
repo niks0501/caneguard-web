89a30e9e5cc192119b124e517485c7cd57231d0c
niks0501 <nikkocausapin61@gmail.com>
2026-07-27 16:04:45 +0800

    feat: enhance test case application creation and storage path management

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -2,9 +2,31 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    private static ?string $testStoragePath = null;
+
+    public function createApplication(): Application
+    {
+        $storagePath = self::$testStoragePath ??= sprintf(
+            '%s/caneguard-web-tests-%s-%d',
+            sys_get_temp_dir(),
+            function_exists('posix_geteuid') ? posix_geteuid() : get_current_user(),
+            getmypid(),
+        );
+
+        $_ENV['LARAVEL_STORAGE_PATH'] = $storagePath;
+        $_SERVER['LARAVEL_STORAGE_PATH'] = $storagePath;
+
+        $compiledViewPath = $storagePath.'/framework/views';
+
+        if (! is_dir($compiledViewPath)) {
+            mkdir($compiledViewPath, 0755, true);
+        }
+
+        return parent::createApplication();
+    }
 }
--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'api/storage/**', 'api/vendor/**']),
   ...query.configs['flat/recommended'],
   {
     files: ['**/*.{ts,tsx}'],
